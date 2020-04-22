aaa06c23e58f0687f0e4fb8a8424a24c102ce92d
Sophia Li <sophiali124@gmail.com>
2020-04-21 17:59:31 -0700

    rad

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 const input = document.getElementById("input");
 const output = document.getElementById("output");
 let multiplier = 0;
+var numQty = require("numeric-quantity");
 
+console.log(numQty("1 1/4") === 1.25);
 // func to split the recipe by line break and join again to an array
 // regex to turn each item into an object
 // convert using formula
 // display output
 
 const ingredientsCupsToGrams = {
-  water: 236,
-  butter: 226,
-  flour: 120,
-  salt: 292,
-  brownSugar: 195,
-  whiteSugar: 200,
-  powderedSugar: 120,
-  honey: 336,
-  buttermilk: 245,
-  milk: 245,
-  oats: 102,
-  bakingSoda: 220,
-  bakingPowder: 220,
-  oil: 218,
-  vanillaExtract: 208,
-  adYeast: 224,
+  water: ["water", 236],
+  butter: ["butter", 226],
+  margarine: ["margarine", 230],
+  // flour: ["flour", 120],
+  APflour: ["flour", 120],
+  cakeFlour: ["cake flour", 114],
+  salt: ["salt", 292],
+  brownSugar: ["brown sugar", 195],
+  whiteSugar: ["white sugar", 200],
+  powderedSugar: ["powdered sugar", 120],
+  honey: ["honey", 336],
+  molasses: ["molasses", 336],
+  syrup: ["syrup", 336],
+  buttermilk: ["buttermilk", 245],
+  milk: ["milks", 245],
+  oats: ["oats", 102],
+  bakingSoda: ["baking soda", 220],
+  bakingPowder: ["baking powder", 220],
+  oil: ["oil", 218],
+  vanillaExtract: ["vanilla extract", 208],
+  activeDryYeast: ["active dry yeast", 224],
 };
+
+const tablespoon = ["tablespoon", "tablespoons", "tbs", "tbs."];
+const teaspoon = ["teaspoon", "teaspoons", "tsp", "tsp."];
+const cup = ["cup", "cups", "c."];
+const gram = ["gram", "grams", "g."];
 
 form.addEventListener("submit", convert);
 
@@ -37,40 +49,94 @@ function convert(event) {
 
   //   regex to make each item into an object using group
   for (let i = 0; i < originalArray.length; i++) {
-    let regex = /(?<qty>\d+) (?<scale>[tablespoon|gram|cup|tsp]+)(\sof\s|\s)(?<ingredient>[\s\S]+)/;
+    // let regex = /(?<qty>\d+) (?<scale>[tablespoon|gram|cup|tsp]+)(\sof\s|\s)(?<ingredient>[\s\S]+)/;
+    let regex = /(?<qty>[^s]+) (?<scale>[tablespoon|gram|cup|tsp]+)(\sof\s|\s)(?<ingredient>[\s\S]+)/;
     parsedItemArray.push(originalArray[i].match(regex).groups);
   }
 
+  // let regex = /(?<qty>[^s]+) (?<scale>[tablespoon|gram|cup|tsp]+)(\sof\s|\s)(?<ingredient>[\s\S]+)/;
+  console.log(parsedItemArray);
   for (let i = 0; i < parsedItemArray.length; i++) {
     //   get multipler of ingredient for conversion formula
     for (let ing = 0; ing < Object.keys(ingredientsCupsToGrams).length; ing++) {
-      let keys = Object.keys(ingredientsCupsToGrams);
+      // let keys = Object.keys(ingredientsCupsToGrams);
       let values = Object.values(ingredientsCupsToGrams);
-      if (parsedItemArray[i].ingredient === keys[ing]) {
-        multiplier = values[ing];
+      if (parsedItemArray[i].ingredient.includes(values[ing][0])) {
+        // console.log(
+        //   parsedItemArray[i].ingredient,
+        //   values[ing][0],
+        //   values[ing][1]
+        // );
+        multiplier = values[ing][1];
       }
     }
 
     // check that ingredient is in our ingredient whitelist
-    if (parsedItemArray[i].ingredient in ingredientsCupsToGrams) {
+    // if (parsedItemArray[i].ingredient in ingredientsCupsToGrams) {
+    if (multiplier) {
       // conversion formula
-      if (parsedItemArray[i].scale === "cups") {
-        let newConversion = Math.round(parsedItemArray[i].qty * multiplier);
-        originalArray[i] = originalArray[i].concat(` (${newConversion} grams)`);
+      // cups to grams
+      for (let j = 0; j < cup.length; j++) {
+        if (parsedItemArray[i].scale == cup[j]) {
+          let newConversion = Math.round(parsedItemArray[i].qty * multiplier);
+          originalArray[i] = originalArray[i].concat(
+            ` (${newConversion} grams)`
+          );
+          multiplier = 0;
+        }
       }
 
       // tablespoons to grams
-      if (parsedItemArray[i].scale === "tablespoon") {
-        let newConversion = Math.round(
-          (parsedItemArray[i].qty * multiplier) / 16
-        );
-        originalArray[i] = originalArray[i].concat(` (${newConversion} grams)`);
+      for (let j = 0; j < tablespoon.length; j++) {
+        if (parsedItemArray[i].scale === tablespoon[j]) {
+          let newConversion = Math.round(
+            (parsedItemArray[i].qty * multiplier) / 16
+          );
+          // console.log(parsedItemArray[i].scale);
+          originalArray[i] = originalArray[i].concat(
+            ` (${newConversion} grams)`
+          );
+          multiplier = 0;
+        }
+      }
+
+      // teaspoon to grams
+      for (let j = 0; j < teaspoon.length; j++) {
+        if (parsedItemArray[i].scale == teaspoon[j]) {
+          let newConversion = (
+            Math.round(parsedItemArray[i].qty * multiplier) /
+            16 /
+            3
+          ).toFixed(2);
+          originalArray[i] = originalArray[i].concat(
+            ` (${newConversion} grams)`
+          );
+          multiplier = 0;
+        }
       }
 
       // grams to cups
-      if (parsedItemArray[i].scale === "grams") {
-        let newConversion = (parsedItemArray[i].qty / multiplier).toFixed(2);
-        originalArray[i] = originalArray[i].concat(` (${newConversion} cups)`);
+      // & parsedItemArray[i].qty < 50
+      // for (let j = 0; j < gram.length; j++) {
+      //   if (parsedItemArray[i].scale === gram[j]) {
+      //     let newConversion = (parsedItemArray[i].qty / multiplier).toFixed(2);
+      //     originalArray[i] = originalArray[i].concat(
+      //       ` (${newConversion} cups)`
+      //     );
+      //     multiplier = 0;
+      //   }
+      // }
+
+      // grams to cups
+      // & parsedItemArray[i].qty < 50
+      for (let j = 0; j < gram.length; j++) {
+        if (parsedItemArray[i].scale === gram[j]) {
+          let newConversion = (parsedItemArray[i].qty / multiplier).toFixed(2);
+          originalArray[i] = originalArray[i].concat(
+            ` (${newConversion} cups)`
+          );
+          multiplier = 0;
+        }
       }
     }
 
